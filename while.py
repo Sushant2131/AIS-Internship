e5fa3b6dd7ac5b7c34d5b2e96cc667029fe7bbb2
sushant chougule <74125735+Sushant2131@users.noreply.github.com>
2023-09-08 08:38:20 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,72 @@
+# 1)odd-even using while loop 
+# i = int(input("Enetr the number :"))
+# while i % 2 == 0:
+#     print(i, "is a even number")
+# else:
+#     print(i, "is a Odd number")
+
+"""
+#2) using while
+* 
+* *
+* * *
+* * * *
+* * * * *
+"""
+
+# i = 1
+# while i <= 5:
+#     print("*" * i)
+#     i=i+1
+
+
+"""
+3) creat list 1-20 numbers list using while loop=> [1,2,3... 20]
+"""
+# i=1
+# a=[]
+# while i<=20:
+#     a.append(i)
+#     i=i+1
+# print(a)
+
+
+"""
+4)  creat list 20-1 (revers order) using while loop=> [20,19...1]
+"""
+# i=1
+# a=[]
+# while i<=20:
+#     a.append(i)
+#     i=i+1
+# print(a[::-1])
+
+
+
+"""
+try with one any eg. break, contnue , pass control statement
+
+"""
+# i=1
+# while i<=10:
+#     i=i+1
+#     print("sushant")
+#     pass
+
+
+
+# i=1
+# while i<=10:
+#     i=i+1
+#     print("sushant")
+#     break
+
+i=1
+while i<=10:
+    i=i+1
+    continue
+    print("sushant")
+    
+    
+
+    
